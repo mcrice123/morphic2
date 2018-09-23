7ee9717949ec88fc032abe071e77c2262b5420a8
Maria Rice <mcrice@shockers.wichita.edu>
2018-09-23 11:34:32 -0500

    Initial commit of a basic website using Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Morphic',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#463299',
+        theme_color: '#463299',
+        display: 'minimal-ui',
+        //icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+      {
+          resolve: `gatsby-plugin-typography`,
+          options: {
+              pathToConfigModule: `src/utils/typography.js`,
+          },
+      },
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Welcome to my site!</h1>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis lectus quis velit bibendum semper. Donec pulvinar, erat at pretium aliquam, lectus turpis finibus felis, ut tristique massa nulla in mi. Sed ac urna lorem. Nunc aliquam risus ex. Duis magna est, porttitor non velit et, porttitor egestas est. Nunc fermentum eu leo ac vehicula. Maecenas volutpat tincidunt nisi, quis faucibus tortor aliquet eget. Sed id sem malesuada massa finibus posuere eget sit amet nisi. Pellentesque quis tellus mi. Integer quis diam ante. Maecenas placerat sem quis mauris ultrices tincidunt. Curabitur dictum urna sit amet turpis aliquam tincidunt. Integer eleifend quam mauris, et tristique arcu suscipit eget. Ut iaculis placerat ullamcorper.</p>
+
+      <h3>This site currently uses Stow Lake, but here are some other themes:</h3>
+      <ul>
+          <li>
+              De Young
+          </li>
+          <li>Lincoln</li>
+      </ul>
+      <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
