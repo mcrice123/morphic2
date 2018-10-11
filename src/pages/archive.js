758c0b42cea21a5a14705e85588534989d60c691
Maria Rice <mcrice@shockers.wichita.edu>
2018-10-11 13:41:00 -0500

    Add tab index to state and add conditional rendering based on each page's category

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -11,9 +11,11 @@ export default class Archive extends Component {
 
         this.state = {
             tabIndex: 0,
+            tabItems: [],
         };
         this.generateTabItems = this.generateTabItems.bind(this);
         this.changeTabs = this.changeTabs.bind(this);
+        this.renderTabs = this.renderTabs.bind(this);
     }
 
     changeTabs(index) {
@@ -40,47 +42,7 @@ export default class Archive extends Component {
         return items;
     }
 
-    renderArchiveQuery() {
-        return (
-            <StaticQuery
-            query={graphql`
-      query archiveQuery {
-        allMarkdownRemark {
-            edges {
-              node {
-                frontmatter {
-                    title
-                    date(formatString: "MMMM DD, YYYY")
-                    categories
-                }
-                fields {
-                    slug
-                 }
-              }
-            }
-        }
-
-      }
-`}
-            render={data =>
-
-                    <ul>
-                        {
-                            data.allMarkdownRemark.edges.map((edge) => {
-                                return <li key={edge.node.fields.slug}>
-                                    <Link to={edge.node.fields.slug}>
-                                        {edge.node.frontmatter.title} [{edge.node.frontmatter.date}]
-                                    </Link>
-                                </li>;
-                            })
-                        }
-                    </ul>
-            }
-        />
-        );
-    }
-
-    renderCategories() {
+    renderTabs() {
         return (
             <StaticQuery
                 query={graphql`
@@ -90,83 +52,67 @@ export default class Archive extends Component {
                         categories
                       }
                     }
+                    allMarkdownRemark {
+                        edges {
+                          node {
+                            frontmatter {
+                                title
+                                date(formatString: "MMMM DD, YYYY")
+                                categories
+                            }
+                            fields {
+                                slug
+                             }
+                          }
+                        }
+                    }
                   }
             `}
                 render={data =>
-                    <TabList
-                        items={this.generateTabItems(data.site.siteMetadata.categories)}
-                    />
+                    <div id={"category-tabs"}>
+                      <TabList
+                          items={this.generateTabItems(data.site.siteMetadata.categories)}
+                      />
+                      <div id={"search-results"}>
+                        <ul>
+                          {
+                            this.state.tabIndex !== data.site.siteMetadata.categories.length
+                            ?
+                            data.allMarkdownRemark.edges.map(edge => {
+                              const allCategories = data.site.siteMetadata.categories;
+                              const { title, date, categories } = edge.node.frontmatter;
+                              const { slug } = edge.node.fields;
+                              const currentTab = allCategories[this.state.tabIndex].toLowerCase().replace(/\s+/g, "");
+                              if (categories.includes(currentTab)) { // going to try filtering with conditional on category stored...
+                              return (
+                                <li key={slug}>
+                                    <Link to={slug}>
+                                        {title} [{date}]
+                                    </Link>
+                                </li>
+                              );
+                              }
+                            })
+                            :
+                            <div>
+                              <input placeholder="Type a character's name"/>
+                            </div>
+                          }
+                        </ul>
+                      </div>
+                    </div>
                 }
             />
         );
     }
-    //}
 
-    render()
-{
-    /*return (
-        <StaticQuery
-            query={graphql`
-      query mainQuery {
-        allMarkdownRemark {
-            edges {
-              node {
-                frontmatter {
-                    title
-                    date(formatString: "MMMM DD, YYYY")
-                    categories
-                }
-                fields {
-                    slug
-                 }
-              }
-            }
-        }
-        site {
-          siteMetadata {
-            categories
-          }
-        }
-      }
-`}
-            render={data =>
-                <Layout>
-                    <h1>Archive</h1>
-                    <div id={"category-tabs"}>
-
-                        <TabList
-                            items={this.generateTabItems(data.site.siteMetadata.categories)}
-                        />
-
-                    </div>
-                    <ul>
-                        {
-                            data.allMarkdownRemark.edges.map((edge) => {
-                                return <li key={edge.node.fields.slug}>
-                                    <Link to={edge.node.fields.slug}>
-                                        {edge.node.frontmatter.title} [{edge.node.frontmatter.date}]
-                                    </Link>
-                                </li>;
-                            })
-                        }
-                    </ul>
-                </Layout>
-            }
-        />
-    );*/
+    render() {
         return (
             <Layout>
                 <h1>Archive</h1>
-                <div id={"category-tabs"}>
-                    {
-                        this.renderCategories()
-                    }
-                </div>
-                <div id={"search-results"}>
-                    {
-                        this.renderArchiveQuery()
-                    }
-                </div>
+                {
+                    this.renderTabs()
+                }
             </Layout>
         );
     }
