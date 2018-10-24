f2030151d763dba588c0c33440e24c54bb0b6538
Maria Rice <mcrice@shockers.wichita.edu>
2018-10-24 18:36:01 -0500

    Add more styling of preview images and the archive page

--- src/components/preview-image.js
+++ src/components/preview-image.js
@@ -5,9 +5,12 @@ require('../components/styles/preview-img.css');
 export default ({path, alt, className, title, date}) => {
   return (
     <div className={className}>
-      <img src={withPrefix(path)} alt={alt} />
-      <p><strong>{title}</strong></p>
-      <p>{date}</p>
+      <div className="main">
+        <img src={withPrefix(path)} alt={alt} />
+        <p><strong>{title}</strong></p>
+        <p>{date}</p>
+      </div>
+      <div className="overlay" />
     </div>
   )
 };
