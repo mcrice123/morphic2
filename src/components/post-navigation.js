babf9aa739a444e2585e49ec9a3755516e80cb41
Maria Rice <mcrice@shockers.wichita.edu>
2018-10-29 13:59:39 -0500

    Create a new component for post navigation and add it to blog post component

---
+++ src/components/post-navigation.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import { StaticQuery, graphql, Link } from "gatsby";
+require('./styles/post-navigation.css');
+
+export default ({currentSlug, data}) => {
+return (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark (
+          sort: {fields: [frontmatter___date], order: ASC},
+        ) {
+          edges {
+            node {
+              frontmatter {
+                title
+                date(formatString: "MMMM DD, YYYY")
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const { edges } = data.allMarkdownRemark;
+
+      let next = {};
+      let previous = {};
+      let current = {
+        slug: currentSlug,
+        title: "",
+      };
+      let index = -1;
+
+      edges.map((edge,i) => {
+        const { node } = edge;
+        const { slug } = node.fields;
+        const { title } = node.frontmatter;
+
+        if (slug === currentSlug) {
+          current.title = title;
+          index = i;
+        }
+        return null;
+      });
+
+      if (index > 0) {
+        let prevNode = edges[index - 1].node;
+        previous = {
+          slug: prevNode.fields.slug,
+          title: prevNode.frontmatter.title,
+        };
+      }
+      else previous = {
+        slug: "",
+        title: "",
+      };
+
+      if (index < edges.length - 1) {
+        let nextNode = edges[index + 1].node;
+        next = {
+          slug: nextNode.fields.slug,
+          title: nextNode.frontmatter.title,
+        };
+      }
+      else next = {
+        slug: "",
+        title: "",
+      };
+      return (
+        <div className="navigation">
+          <Link to={previous.slug}>
+            <div className="previous link">
+              {previous.title}
+            </div>
+          </Link>
+          <h3 className="title">{current.title}</h3>
+          <Link to={next.slug}>
+            <div className="next link">
+              {next.title}
+            </div>
+          </Link>
+        </div>
+      );
+    }
+
+    }
+    />
+  );
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,20 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql} from "gatsby";
+import { graphql, Link } from "gatsby";
 
 import MainImage from '../components/main-image';
+import PostNavigation from '../components/post-navigation';
+//require('../components/styles/blog-post.css');
 
 export default ({data}) => {
     const post = data.markdownRemark;
     const { title, date, featuredpath } = post.frontmatter;
+    const { slug } = post.fields;
     return (
         <Layout>
             <div>
                 <MainImage path={featuredpath} alt={title} />
+                <PostNavigation currentSlug={slug}/>
                 <h1>{title}</h1>
                 <h3>{date}</h3>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -27,6 +31,9 @@ export const query = graphql`
         title
         date(formatString: "MMMM DD, YYYY")
         featuredpath
+      }
+      fields {
+        slug
       }
     }
   }
