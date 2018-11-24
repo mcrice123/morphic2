4da4e8b381dc6ae2919af39e6ec431f8fd419508
Maria Rice <mcrice@shockers.wichita.edu>
2018-11-24 11:34:17 -0600

    Add basic list rendering using hard-coded list of strings on Character page

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Layout from '../components/layout';
-export default () => {
-    return (
+export default class Characters extends Component {
+
+    renderList() {
+        const testList = ["a", "b", "c"];
+        return (
+            <ul>
+              {
+                testList.map(item => {
+                  return <li>{item}</li>;
+                })
+              }
+            </ul>
+        );
+    }
+
+    render() {
+      return (
         <Layout>
-            <div>Characters</div>
+            <h1>Characters</h1>
+            {
+              this.renderList()
+            }
         </Layout>
     );
-};
+  }
+}
