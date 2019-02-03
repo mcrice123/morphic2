8ee3b95e4e6839e8429b5909449e2100d21ccf71
Maria Rice <mcrice@shockers.wichita.edu>
2019-02-03 17:04:48 -0600

    Add more styling to make about page responsive

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,13 +10,13 @@ const About = () => {
     return (
         <Layout>
             <div id="about">
-                <div>
+                <div id="text-container">
                     <h1>Morphic</h1>
                     <h3>A Graphic Novel Series by Maria Rice</h3>
                     <p>
                     <em>Change comes quickly to Errie's home town when she stumbles into a quarrel 
                         among immortal beings previously unknown to humans-- </em> <b>shape-shifters called <em>morphics</em>,
-                        who take the form of the animal they resemble.</b>
+                        who take the form of the animals.</b>
                     </p>
                     <p>
                         <b>My name is Maria Rice</b> and I have been uploading pages for "Morphic" since August of 2016. 
@@ -26,14 +26,16 @@ const About = () => {
                         "Morphic" is intended to be a <b>color-printed series of 3 books</b> when it is finished. 
                         As of writing this description, it is currently in Book 2. 
                         While continuing to upload new colored pages, I am working to update older, uncolored pages. 
-                        <b>I draw the pages with black ink pens and color them digitally 
+                         <b> I draw the pages with black ink pens and color them digitally 
                             with <a target="_blank" href="https://www.gimp.org/">GIMP</a>.</b>
                     </p>
                     <p>
 
                     </p>
                 </div>
-                <img src={AboutImage} alt="Errie and the Vines" />
+                <div id="img-container">
+                    <img src={AboutImage} alt="Errie and the Vines" />
+                </div>
             </div>
         </Layout>
     );
