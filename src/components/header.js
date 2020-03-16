291a2b0faeeeec57e27a427fd5e8121eaf4e6b44
Maria Rice <maria@lifeboatcreative.com>
2020-03-16 07:52:32 -0500

    Redesign navbar and banner

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import MenuIcon from './menuicon';
 import SlideMenu from './slidemenu';
 import BannerImage from '../images/banner.png';
+import MorphicLogo from "../assets/morphic_logo.svg";
 require('./styles/header.scss');
 
 class Header extends Component {
@@ -12,45 +13,42 @@ class Header extends Component {
         const navLinks = this.props.navLinks;
         return (
             <div id={"header"}>
-                <div id={"header-banner"}>
-                    <img src={BannerImage} alt="Morphic" />
+                <div id={"header-banner"} style={{ backgroundImage: `url(` + BannerImage + `)` }}>
                 </div>
                 <div id={"header-links"} onClick={this.props.closeMenu}>
-                    <div className={"title"}>
-                        <h1>
-                            <Link to="/">
-                                {siteTitle}
-                            </Link>
-                        </h1>
+                    <div className={"wrapper"}>
+                        <Link id={"main-logo"} to="/">
+                            <MorphicLogo />
+                        </Link>
+                        <nav id={"navbar"}>
+                            <ul>
+                                {
+                                    navLinks.map((myLink) => {
+                                        const path = "/" + (myLink !== "Home" ? myLink.toLowerCase() + "/" : "");
+                                        return (<li key={myLink}>
+                                                <Link to={path}>
+                                                    {myLink}
+                                                </Link>
+                                            </li>
+                                        );
+                                    })
+                                }
+                            </ul>
+                        </nav>
+                        <div
+                            id={"menuIcon"}
+                            onClick={e => { this.props.toggleMenu(); e.stopPropagation();}}
+                            style={{ backgroundColor: this.props.menuOpen ? 'rgba(255, 255, 255, .2)' : 'transparent'}}
+                        >
+                            <MenuIcon/>
+                        </div>
+                        {
+                            this.props.menuOpen &&
+                            <SlideMenu
+                                navLinks={navLinks}
+                            />
+                        }
                     </div>
-                    <nav id={"navbar"}>
-                        <ul>
-                            {
-                                navLinks.map((myLink) => {
-                                    const path = "/" + (myLink !== "Home" ? myLink.toLowerCase() + "/" : "");
-                                    return (<li key={myLink}>
-                                            <Link to={path}>
-                                                {myLink}
-                                            </Link>
-                                        </li>
-                                    );
-                                })
-                            }
-                        </ul>
-                    </nav>
-                    <div
-                        id={"menuIcon"}
-                        onClick={e => { this.props.toggleMenu(); e.stopPropagation();}}
-                        style={{ backgroundColor: this.props.menuOpen ? 'rgba(255, 255, 255, .2)' : 'transparent'}}
-                    >
-                        <MenuIcon/>
-                    </div>
-                    {
-                        this.props.menuOpen &&
-                        <SlideMenu
-                            navLinks={navLinks}
-                        />
-                    }
                 </div>
             </div>
         );
