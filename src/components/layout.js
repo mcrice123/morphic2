1670aecacaa6681d9ab422abf135c7a50a2e2a6c
Maria Rice <good2sew@gmail.com>
2020-09-05 15:40:44 -0500

    Declare variables for sitemetadata and set og:url attribute

--- src/components/layout.js
+++ src/components/layout.js
@@ -55,100 +55,115 @@ class Layout extends Component {
                         }
                     `}
 
-            render={data => (
-                <>
-                    <Helmet
-                        title={data.site.siteMetadata.title}
-                        meta={[
-                            {name: 'description', content: `${data.site.siteMetadata.description}`},
-                            {name: 'keywords', content: `${data.site.siteMetadata.keywords}`},
-                            {name: 'image', content: `${data.site.siteMetadata.siteUrl}` + '/seo/default.jpg'},
-                            {
-                                property: `og:title`,
-                                content: data.site.siteMetadata.title,
-                            },
-                            {
-                                property: `og:description`,
-                                content: data.site.siteMetadata.description,
-                            },
-                            {
-                                property: `og:type`,
-                                content: `website`,
-                            },
-                            {
-                                name: `twitter:creator`,
-                                content: data.site.siteMetadata.author,
-                            },
-                            {
-                                name: `twitter:title`,
-                                content: data.site.siteMetadata.title,
-                            },
-                            {
-                                name: `twitter:description`,
-                                content: data.site.siteMetadata.description,
-                            },
-                        ]
-                        .concat(
-                        //    metaImage
-                        //      ? [
-                            [
-                                  {
-                                    property: "og:image",
-                                    content: `${data.site.siteMetadata.siteUrl}` + '/seo/default.jpg',
-                                  },
-                                  {
-                                    property: "og:image:width",
-                                    content: 1200,
-                                  },
-                                  {
-                                    property: "og:image:height",
-                                    content: 630,
-                                  },
-                                  {
-                                    name: "twitter:card",
-                                    content: "summary_large_image",
-                                  },
-                                ]
-                             // : [
-                            //      {
-                            //        name: "twitter:card",
-                            //        content: "summary",
-                            //      },
-                            //    ]
-                        )}
-                        link={[
-                            { rel: "icon", type: "image/png", sizes: "16x16", href: `${Favicon}` },
-                            { rel: "icon", type: "image/png", sizes: "32x32", href: `${Favicon}` },
-                            { rel: "shortcut icon", type: "image/png", href: `${Favicon}` },
-                          ]}
-                    >
-                        <html lang="en"/>
-                    </Helmet>
-                    <Header
-                        siteTitle={data.site.siteMetadata.title}
-                        navLinks={data.site.siteMetadata.navLinks}
-                        toggleMenu={this.toggleMenu}
-                        closeMenu={this.closeMenu}
-                        menuOpen={this.state.menuOpen}
-                    />
-                    <BackToTop />
-                    <div
-                         id={"allcontent"}
-                         onClick={this.closeMenu}
-                         style={{
-                             margin: '0 auto',
-                             maxWidth: 960,
-                             padding: '0px 1.0875rem 1.45rem',
-                             paddingTop: 0,
-                         }}
-                    >
-                        {this.props.children}
-                        <div id="copyright">&copy; Maria Rice <CurrentYear /></div>
-                    </div>
-                </>
-            )}
-        />
-        </div>
+                    render={
+                        data => {
+                            let title = data.site.siteMetadata.title;
+                            let description = data.site.siteMetadata.description;
+                            let keywords = data.site.siteMetadata.keywords;
+                            let siteUrl = data.site.siteMetadata.siteUrl;
+                            let author = data.site.siteMetadata.author;
+                            return (
+                                <>
+                                    <Helmet
+                                        title={title}
+                                        meta={
+                                            [
+                                                {name: 'description', content: description},
+                                                {name: 'keywords', content: keywords},
+                                                {name: 'image', content: siteUrl + '/seo/default.jpg'},
+                                                {
+                                                    property: `og:title`,
+                                                    content: title,
+                                                },
+                                                {
+                                                    property: `og:description`,
+                                                    content: description,
+                                                },
+                                                {
+                                                    property: `og:type`,
+                                                    content: `website`,
+                                                },
+                                                {
+                                                    property: `og:url`,
+                                                    content: siteUrl,
+                                                },
+                                                {
+                                                    name: `twitter:creator`,
+                                                    content: author,
+                                                },
+                                                {
+                                                    name: `twitter:title`,
+                                                    content: title,
+                                                },
+                                                {
+                                                    name: `twitter:description`,
+                                                    content: description,
+                                                },
+                                            ]
+                                            .concat(
+                                            //    metaImage
+                                            //      ? [
+                                                [
+                                                    {
+                                                        property: "og:image",
+                                                        content: siteUrl + '/seo/default.jpg',
+                                                    },
+                                                    {
+                                                        property: "og:image:width",
+                                                        content: 1200,
+                                                    },
+                                                    {
+                                                        property: "og:image:height",
+                                                        content: 630,
+                                                    },
+                                                    {
+                                                        name: "twitter:card",
+                                                        content: "summary_large_image",
+                                                    },
+                                                    ]
+                                                // : [
+                                                //      {
+                                                //        name: "twitter:card",
+                                                //        content: "summary",
+                                                //      },
+                                                //    ]
+                                            )
+                                        }
+                                        link={[
+                                            { rel: "icon", type: "image/png", sizes: "16x16", href: `${Favicon}` },
+                                            { rel: "icon", type: "image/png", sizes: "32x32", href: `${Favicon}` },
+                                            { rel: "shortcut icon", type: "image/png", href: `${Favicon}` },
+                                        ]}
+                                    >
+                                        <html lang="en"/>
+                                    </Helmet>
+                                    <Header
+                                        siteTitle={title}
+                                        navLinks={data.site.siteMetadata.navLinks}
+                                        toggleMenu={this.toggleMenu}
+                                        closeMenu={this.closeMenu}
+                                        menuOpen={this.state.menuOpen}
+                                    />
+                                    <BackToTop />
+                                    <div
+                                        id={"allcontent"}
+                                        onClick={this.closeMenu}
+                                        style={{
+                                            margin: '0 auto',
+                                            maxWidth: 960,
+                                            padding: '0px 1.0875rem 1.45rem',
+                                            paddingTop: 0,
+                                        }}
+                                    >
+                                        {this.props.children}
+                                        <div id="copyright">&copy; Maria Rice <CurrentYear /></div>
+                                    </div>
+                                </>
+                            );
+                        }
+                    }
+                />
+            </div>
         );
     }
 }
