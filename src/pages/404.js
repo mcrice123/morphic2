ed1a8b80eed8139818a4255b2ba7de50bfc90d8a
Maria Rice <good2sew@gmail.com>
2020-09-10 14:20:22 -0500

    Update 404 page with updated content and new image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
-
+import Embed from '../../static/embed/a-little-lost.jpg'
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="post-body">
+      <h1>A little lost?</h1>
+      <p>You just hit a route that doesn&#39;t exist.</p>
+      <img src={Embed} alt="Errie and Capri, the goat, look confused. Errie says 'Awkward...'" />
+    </div>
   </Layout>
 )
 
