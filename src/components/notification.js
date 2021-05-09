5d037a2a2bf95c663e20977c17f098d55f4b1453
Maria Rice <good2sew@gmail.com>
2021-05-08 21:32:03 -0500

    Forgot to update link in notification banner--it now points to 2.75

--- src/components/notification.js
+++ src/components/notification.js
@@ -3,7 +3,7 @@ import { Link, withPrefix } from 'gatsby'
 
 export default ({style}) => {
     return (
-        <Link to="https://www.morphicwebcomic.com/book2/book-2-page-74/" id={"notification-banner"}>
+        <Link to="https://www.morphicwebcomic.com/book2/book-2-page-75/" id={"notification-banner"}>
             <span style={style}>
                 <span><strong>NEXT POST:</strong>&nbsp;&nbsp;&nbsp;Saturday, June 5th &#124; <strong>Book 2 Completed</strong></span>
                 <span className="highlight-btn">Read Latest Page</span>
