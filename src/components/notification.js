791b108272ae2eca39308880929254af6b312c2a
Maria Rice <good2sew@gmail.com>
2021-06-05 23:51:32 -0500

    Update button text from 'page' to 'post'

--- src/components/notification.js
+++ src/components/notification.js
@@ -6,7 +6,7 @@ export default ({style}) => {
         <Link to="https://www.morphicwebcomic.com/misc/2021-june/" id={"notification-banner"}>
             <span style={style}>
                 <span><strong>NEXT POST:</strong>&nbsp;&nbsp;&nbsp;Saturday, July 10th &#124; <strong>Book 2 Completed</strong></span>
-                <span className="highlight-btn">Read Latest Page</span>
+                <span className="highlight-btn">Read Latest Post</span>
             </span>
         </Link>
     );
