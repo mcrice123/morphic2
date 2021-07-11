892471fc1a16066b8059f74e8c39a612fcd7fc9f
Maria Rice <good2sew@gmail.com>
2021-07-10 19:40:30 -0500

    Create new post for July 2021 cosplay artwork

--- src/components/notification.js
+++ src/components/notification.js
@@ -3,9 +3,9 @@ import { Link, withPrefix } from 'gatsby'
 
 export default ({style}) => {
     return (
-        <Link to="https://www.morphicwebcomic.com/misc/2021-june/" id={"notification-banner"}>
+        <Link to="https://www.morphicwebcomic.com/misc/2021-july/" id={"notification-banner"}>
             <span style={style}>
-                <span><strong>NEXT POST:</strong>&nbsp;&nbsp;&nbsp;Saturday, July 10th &#124; <strong>Book 2 Completed</strong></span>
+                <span><strong>NEXT POST:</strong>&nbsp;&nbsp;&nbsp;Saturday, August 7th &#124; <strong>Book 2 Completed</strong></span>
                 <span className="highlight-btn">Read Latest Post</span>
             </span>
         </Link>
